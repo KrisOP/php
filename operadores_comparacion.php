f90f13bdb558ab8fff7d9e60e1fd905bd3e66a0d
KrisOP <kortega070@gmail.com>
2018-11-02 10:04:48 -0600

    se agrego el archivo operadores_comparacion.php

---
+++ operadores_comparacion.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $var1=8;
+        $var2="8";
+        $var3="Juan";
+
+        if ($var3!=$var2)
+        {
+            echo "No son iguales";
+        }
+        else
+        {
+            echo " son iguales";
+        }
+    ?>
+</body>
+</html>
