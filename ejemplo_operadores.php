d9a610cac8b96b04939b761d8cf90075481622ee
KrisOP <kortega070@gmail.com>
2018-11-03 15:14:49 -0600

    se agrego el codigo php para calcular

--- ejemplo_operadores.php
+++ ejemplo_operadores.php
@@ -8,7 +8,7 @@
 <body>
 
 <p>&nbsp;</p>
-<form name="form1" method="post" action="calculadora.php">
+<form name="form1" method="post" action="">
   <p>
     <label for="num1"></label>
     <input type="text" name="num1" id="num1">
@@ -18,9 +18,9 @@
     <select name="operacion" id="operacion">
       <option>Suma</option>
       <option>Resta</option>
-      <option>Multiplicación</option>
-      <option>División</option>
-      <option>Módulo</option>
+      <option>Multiplicacion</option>
+      <option>Division</option>
+      <option>Modulo</option>
     </select>
   </p>
   <p>
@@ -29,8 +29,40 @@
 </form>
 <p>&nbsp;</p>
 
+<?php
+  if (isset($_POST["button"]))
+  {
+    $numero1=$_POST["num1"];
+    $numero2=$_POST["num2"];
+    $operacion=$_POST["operacion"];
 
+    
+    if(!strcmp("Suma",$operacion))
+    {
+      echo "El resultado de la suma es: ". ($numero1+$numero2);
+    }
+    if(!strcmp("Resta",$operacion))
+    {
+      echo "El resultado de la resta es: ". ($numero1-$numero2);
+    }
 
+    if(!strcmp("Multiplicacion",$operacion))
+    {
+      echo "El resultado de la multiplicacion es: ". ($numero1*$numero2);
+    }
+
+    if(!strcmp("Division",$operacion))
+    {
+      echo "El resultado de la division es: ". ($numero1/$numero2);
+    }
+
+    if(!strcmp("Modulo",$operacion))
+    {
+      echo "El resultado de la division es: ". ($numero1%$numero2);
+    }
+
+  }
+?>
 
 </body>
 </html>
