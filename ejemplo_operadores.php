11bb0169d1766d51d41ae289c8f5dc30c8ebc932
KrisOP <kortega070@gmail.com>
2018-11-03 15:23:40 -0600

    se agrego archivo ejemplo_operadores.html (con etiquetas de ejemplo_operadores.php)

--- ejemplo_operadores.php
+++ ejemplo_operadores.php
@@ -29,7 +29,40 @@
 </form>
 <p>&nbsp;</p>
 
+<?php
+  if (isset($_POST["button"]))
+  {
+    $numero1=$_POST["num1"];
+    $numero2=$_POST["num2"];
+    $operacion=$_POST["operacion"];
 
+    
+    if(!strcmp("Suma",$operacion))
+    {
+      echo "El resultado de la suma es: ". ($numero1+$numero2);
+    }
+    if(!strcmp("Resta",$operacion))
+    {
+      echo "El resultado de la resta es: ". ($numero1-$numero2);
+    }
+
+    if(!strcmp("Multiplicacion",$operacion))
+    {
+      echo "El resultado de la multiplicacion es: ". ($numero1*$numero2);
+    }
+
+    if(!strcmp("Division",$operacion))
+    {
+      echo "El resultado de la division es: ". ($numero1/$numero2);
+    }
+
+    if(!strcmp("Modulo",$operacion))
+    {
+      echo "El resultado de la division es: ". ($numero1%$numero2);
+    }
+
+  }
+?>
 
 </body>
 </html>
