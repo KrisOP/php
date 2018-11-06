65bd02ed5c2269b5bfb102fa128f7bde0712e38d
KrisOP <kortega070@gmail.com>
2018-11-06 09:04:42 -0600

    bucles do while y while

---
+++ bucles.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $var1=1;
+    //Practica del While (Bucle indeterminados)
+    while ($var1<6)
+    {
+        echo "Estamos ejecutando el codigo del bucle while <br>";
+        $var1++;
+    }
+
+    echo "<br> Hemos salido del bucle <br> <br>";
+
+    //practica del do while (bucle indeterminados)
+    do 
+    {
+        echo "Estamos en el do While <br>";
+        $var1--; 
+
+    }while($var1>1)
+
+
+    //Practica del for (bucle determinado)
+    ?>
+
+</body>
+</html>
