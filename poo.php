14e4270ef21320917535f05631b4fa5840ae864d
KrisOP <kortega070@gmail.com>
2018-11-06 22:57:41 -0600

    creando e instanciando clases

---
+++ poo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Las clases es mejor el nombre la primera letra es mayuscula
+        class Coche 
+        {
+
+
+        }
+
+        $toyota = new Coche();//instancia perteneciente a la clase coche
+        $tesla = new Coche();
+    ?>
+</body>
+</html>
