1aa52d978491757bc04c299577eb18513598111d
KrisOP <kortega070@gmail.com>
2018-11-07 10:31:03 -0600

    se agrego el archivo auto.php

---
+++ auto.php
@@ -0,0 +1,36 @@
+<?php
+//Las clases es mejor el nombre la primera letra es mayuscula
+        class Coche 
+        {
+            var $ruedas;//declarando atribustos de  la clase coche
+            var $color;
+            var $motor;
+
+            function Coche ()//cuando un metodo dentro de una clase tenga el mismo nombre de la clase es un metodo constructor
+            {
+                $this-> ruedas=4;//referencia a la clase coche //En su estado inicial tiene 4 ruedas
+                $this-> color="";
+                $this-> motor=1600;
+            }
+
+            function arrancar ()//declarando funcionalidades
+            {
+                echo "El corre arranca";
+            }
+
+            function girar ()
+            {
+                echo "El coche gira";
+            }
+            function frenar ()
+            {
+                echo "El coche frena";
+            }
+
+            function establece_color($color_coche,$nombre_coche)
+            {
+                $this->color=$color_coche;
+                echo "El color de ". $nombre_coche ." es: ". $this->color;
+            }
+        }
+?>
