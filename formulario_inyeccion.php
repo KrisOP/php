52b721c144fdb4fa60966f9a53ebcdcd0835ae9c
KrisOP <kortega070@gmail.com>
2018-11-26 23:28:31 -0600

    se agregaron archivos para hacer pruebas de inyeccion mysql

---
+++ formulario_inyeccion.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="consulta_inyeccion.php" method="get">
+        <label>Ingrese el pais a buscar: <input type="text" name="pais"></label><br>
+        <input type="submit" name="enviando" value="buscar por pais">
+        
+    </form>
+</body>
+</html>
