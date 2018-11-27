5010794ddc78a7e118b0048441ea84a272bc7c2a
KrisOP <kortega070@gmail.com>
2018-11-26 18:07:22 -0600

    mostranto resutados en caja de textoss

--- formulario_actualizar.php
+++ formulario_actualizar.php
@@ -1,72 +1,16 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Documento sin título</title>
-<style>
-
-h1{
-	text-align:center;
-	color:#00F;
-	border-bottom:dotted #0000FF;
-	width:50%;
-	margin:auto;
-	
-	
-}
-
-table{
-	border:1px solid #F00;
-	padding:20px 50px;
-	margin-top:50px;
-}
-
-body{
-	background-color:#FFC;
-}
-
-
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
-
 <body>
-<h1>Eliminar Artículos</h1>
-<form name="form1" method="get" action="eliminar_registro.php">
-  <table border="0" align="center">
-    <tr>
-      <td>Sección</td>
-      <td><label for="seccion"></label>
-      <input type="text" name="seccion" id="seccion"></td>
-    </tr>
-    <tr>
-      <td>Nombre artículo</td>
-      <td><label for="n_art"></label>
-      <input type="text" name="n_art" id="n_art"></td>
-    </tr>
-    <tr>
-      <td>Precio</td>
-      <td><label for="precio"></label>
-      <input type="text" name="precio" id="precio"></td>
-    </tr>
-    <tr>
-      <td>Fecha</td>
-      <td><label for="fecha"></label>
-      <input type="text" name="fecha" id="fecha"></td>
-    </tr>
-    <tr>
-      <td>País de Origen</td>
-      <td><label for="p_orig"></label>
-      <input type="text" name="p_orig" id="p_orig"></td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-    </tr>
-    <tr>
-      <td align="center"><input type="submit" name="enviar" id="enviar" value="Eliminar"></td>
-      <td align="center"><input type="reset" name="Borrar" id="Borrar" value="Borrar"></td>
-    </tr>
-  </table>
-</form>
+    <form action="pag_resultados_actualizar.php" method="get">
+    <label>Buscar: <input type="text" name="buscar"></label>
+
+    <input type="submit" name="enviando" value="Dale!">
+
 </body>
 </html>
---
+++ pag_resultados_actualizar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+   $busqueda=$_GET["buscar"];//almacenar lo que se esta pasando del cuadro de texto buscar 
+   require("conexion.php");
+   //$conexion=mysqli_connect($db_host,$db_usuario,$db_contra,$db_nombre);
+
+    $conexion=mysqli_connect($db_host,$db_usuario,$db_contra);
+
+    if (mysqli_connect_errno())//si existe algun error de conexion
+    {
+        echo "Fallo al conectar con la BBDD";
+        exit();
+    }
+    
+    mysqli_select_db($conexion,$db_nombre) or die ("No se encuentra la base de datos");
+
+    mysqli_set_charset($conexion,"utf8");//para que se reconozca los simbolos latinos como el acento, la e;e
+    $consulta="select * from productos where NOMBREARTICULO LIKE'%$busqueda%'";
+
+    $resultado=mysqli_query($conexion,$consulta);
+
+    //acceder la primera vez al primer registro pero en un bucle accede a cada uno de los registros
+   // $fila=mysqli_fetch_row($resultado); //recorre fila a fila lo almacenado en el resultado (tabla virtual)
+    
+    $registros=1;
+
+    //while(($fila=mysqli_fetch_row($resultado))==true)//dejara de acceder cuando no haya informacion
+    while(($fila=mysqli_fetch_array($resultado, MYSQL_ASSOC)))//Con MYSQL_ASSOC decimos que queremos trabajar con arreglos asociativos
+    {
+        //echo "<table width'50%' align='center' border='1px' dotted #FF000 margin='auto'> <tr> <td>";
+        //$fila=mysqli_fetch_row($resultado);//
+        
+        echo "<form action='actualizar.php' method='get'>";//como le vamoss a pasar a traves de la url usamos el metodo get
+
+        echo "Seccion " . "<input type='text' name='seccion' value='" . $fila['SECCION']. "'><br>";
+
+        echo "Nombre Articulo ". "<input type='text' name='n_art' value='" . $fila['NOMBREARTICULO']. "'><br>";
+
+        echo "Pais de origen ". "<input type='text' name='pais' value='" . $fila['PAIS']. "'><br>";
+
+
+        echo "<input type='submit' name='enviando' value='Actualizar!'>";
+
+        echo"</form>";
+        echo"<br>";
+    }
+
+    //para cerrar la conexxion para optimizar recursos
+    mysqli_close($conexion);
+
+    ?>
+</body>
+</html>
