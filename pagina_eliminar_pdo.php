bbb391bab89a95d0ad7fed4bf0303a954467ff64
KrisOP <kortega070@gmail.com>
2018-11-28 15:56:47 -0600

    agregando codigo para eliminar con pdo

--- pagina_eliminar_pdo.php
+++ pagina_eliminar_pdo.php
@@ -17,16 +17,17 @@
             $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//decir a la BD que tome como propiedades las excepciones que sera capaz de crearlas en el caso que se produzca un error. Y podemos rescatar propiedades y metodos del objeto error//Osea podemos ver el tipo de error para identificarlo y solucionarlo facilmente
             $base->exec("SET CHARACTER SET utf8");
             //$sql="SELECT NOMBREARTICULO,SECCION,PRECIO,PAIS FROM productos where SECCION= :SECC AND PAIS= :PORIG";//uso del marcador
-            $sql="INSERT INTO productos (SECCION, NOMBREARTICULO,FECHA,PAIS,PRECIO) VALUES (:seccion,:n_art,:fecha,:pais,:precio)";//HACIENDO USO DE LOSS MARCADORES
-            
+            //$sql="INSERT INTO productos (SECCION, NOMBREARTICULO,FECHA,PAIS,PRECIO) VALUES (:seccion,:n_art,:fecha,:pais,:precio)";//HACIENDO USO DE LOSS MARCADORES
+            $sql="DELETE FROM productos WHERE NOMBREARTICULO =:n_art";
             $resultados=$base->prepare($sql);//devuelve un objeto de tipo PDO almacenado en resultado
             //$resultados->execute(array(":SECC"=>$seccion, ":PORIG"=>$pais));//ejecutar consulta y pasarle al marcador lo almacenado en $buscar
-            $resultados->execute(array(":seccion"=>$seccion,":n_art"=>$n_art,":fecha"=>$fecha,":pais"=>$pais,"precio"=>$precio));//asociar marcadores con variables
-           /* while ($registro=$resultados->fetch(PDO::FETCH_ASSOC))
+            //$resultados->execute(array(":seccion"=>$seccion,":n_art"=>$n_art,":fecha"=>$fecha,":pais"=>$pais,"precio"=>$precio));//asociar marcadores con variables
+            $resultados->execute(array(":n_art"=>$n_art));
+            /* while ($registro=$resultados->fetch(PDO::FETCH_ASSOC))
             {
                 echo "Nombre Articulo " . $registro['NOMBREARTICULO'] . "SECCION: ". $registro['SECCION'] . "PRECIO " . $registro['PRECIO'] . "PAIS" . $registro['PAIS'] . "<br>";
             }*/
-            echo "Registros insertados";
+            echo "Registros Eliminados";
             //cerrar el cursor
             $resultados->closeCursor();
         }catch (Excepcion $e)
