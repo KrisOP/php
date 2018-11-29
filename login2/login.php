2a59b7cfccec4973b30a23f8672ad703667c2751
KrisOP <kortega070@gmail.com>
2018-11-29 06:35:56 -0600

    agregando nuevo sistema de login

---
+++ login2/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Introduce tus datos<h1>
+
+    <form action="comprueba_login.php" method="post">
+    <table>
+    <tr>
+    <td class="izq">
+    Login:</td><td class="der"><input type="text" name="login"></td></tr>
+    <tr><td class="izq">Password:</td><td class="der"><input type="password" name="password"></td></tr>
+    <tr><td coldspan="2"><input type="submit" name="enviar" value="login"></td></tr></table>
+
+</body>
+</html>
