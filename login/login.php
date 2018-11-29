21cc06897a8af1a3f6f2b30cca81a054bbd1c56b
KrisOP <kortega070@gmail.com>
2018-11-28 20:11:53 -0600

    etiquetado del login

--- login/login.php
+++ login/login.php
@@ -7,6 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Introduce tus datos<h1>
+
+    <form action="comprueba_login.php" method="post">
+    <table>
+    <tr>
+    <td class="izq">
+    Login:</td><td class="der"><input type="text" name="login"></td></tr>
+    <tr><td class="izq">Password:</td><td class="der"><input type="password" name="password"></td></tr>
+    <tr><td coldspan="2"><input type="submit" name="enviar" value="login"></td></tr></table>
+
 </body>
 </html>
