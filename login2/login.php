808086bc18b650f37039e64f07ccee165bfcfad8
KrisOP <kortega070@gmail.com>
2018-11-29 06:40:43 -0600

    usando ['PHP_SELF'] para hacer una peticion al servidor y cargar la propia pagina

--- login2/login.php
+++ login2/login.php
@@ -9,13 +9,17 @@
 <body>
     <h1>Introduce tus datos<h1>
 
-    <form action="comprueba_login.php" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF'];//hacer una peticion al servidor de la carga de la propia pagina?>" method="post">
     <table>
     <tr>
     <td class="izq">
     Login:</td><td class="der"><input type="text" name="login"></td></tr>
     <tr><td class="izq">Password:</td><td class="der"><input type="password" name="password"></td></tr>
     <tr><td coldspan="2"><input type="submit" name="enviar" value="login"></td></tr></table>
+</form>
+
+<h2>Contenido de la WEB</h2>
+<p>Este es el contenido de la WEB</p>
 
 </body>
 </html>
