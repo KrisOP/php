7092876fd5a853bf413cb8da32ac160e7bf993c9
KrisOP <kortega070@gmail.com>
2018-11-28 18:13:18 -0600

    haciendo conexion a BBDD con PDO

--- POO/conexion.php
+++ POO/conexion.php
@@ -8,14 +8,32 @@
 
         public function Conexion()//metodo constructor
         {
-            //$this->conexion_db=new mysqli('localhost','root', '12345678', 'pruebas');
+           
+           
+            //**************** */CONEXION CON MYSQLI*****************
+            /*//$this->conexion_db=new mysqli('localhost','root', '12345678', 'pruebas');
             $this->conexion_db=new mysqli(DB_HOST,DB_USUARIO, DB_CONTRA, DB_NOMBRE);
             if ($this->conexion_db->connect_errno)//si existe un error
             {
                 echo "fallo al conectar a Mysql: ". $this->conexion_db->connect_error;
                 return;
             }
-            $this->conexion_db->set_charset(DB_CHARSET);//juego de caracteres para que podamos utilizar la ñ
+            $this->conexion_db->set_charset(DB_CHARSET);//juego de caracteres para que podamos utilizar la ñ*/
+           
+            
+            
+            //**************** */CONEXION CON PDO*****************
+            try {
+              
+                    $this->$conexion_db=new PDO ('mysql:host=localhost;dbname=pruebas','root','12345678');//creando conexion
+                    $this->$conexion_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                    $this  ->$conexion_db->exec("SET CHARACTER SET utf8");
+                    return $this->$conexion_db;
+            } catch (Exception $e) {
+
+                echo "La linea del error es: ". $e->getLine();
+                
+            }
         }
     }
 ?>
