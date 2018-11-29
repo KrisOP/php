f3781bc4442ddfc175005c547dd0392523cd41ec
KrisOP <kortega070@gmail.com>
2018-11-29 03:14:27 -0600

    se agregaron usuarios_registrados2,3y4 para navegar entre paginas y cerrar sesion

---
+++ login/usuarios_registrados3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    session_start();//renaurar sesion si ya se ha iniciado una. Si, es asi, se puede acceder al valor de la variable super globlar cuando se creo la sesion
+
+    if(!isset($_SESSION["usuario"]))//si no hay algo almacenado en la variable superglobal
+    {
+        header("Location:login.php");//si no hay inicio de sesion al introducir la url de esta pagina, redirecciona a login.php
+    }
+    ?>
+    <h1>Bienvenido Usuarios</h1>
+
+    <?php
+    echo "Hola: ". $_SESSION["usuario"];
+    ?>
+</body>
+</html>
