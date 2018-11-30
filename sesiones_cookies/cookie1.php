3b6cf32117ecea966e450c2086cb8d743286dbf9
KrisOP <kortega070@gmail.com>
2018-11-29 20:51:24 -0600

    se agrego archivo leer_cookie.ph

---
+++ sesiones_cookies/cookie1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //se creara y guardara la cookie en el disco de la pc
+        setcookie("prueba","esta es la informacion de nuestra primera cookie");//Nombre de la cookie es prueba, y el valor o descripcion
+
+    ?>
+</body>
+</html>
