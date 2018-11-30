3859418c2c2a7da2dee38db17ca1731fd4b30f43
KrisOP <kortega070@gmail.com>
2018-11-30 03:49:29 -0600

    se agrego creaCookie.php

--- sesiones_cookies/practicas_cookies/creaCookie.php
+++ sesiones_cookies/practicas_cookies/creaCookie.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pagina principal</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Elije un idioma</h1>
-
-    <a href="creaCookie.php?idioma=es"> Idioma Espanol</a><br>
-    <a href="creaCookie.php?idioma=en"> Idioma Ingles</a>
+    <?php
+    ?>
 </body>
 </html>
