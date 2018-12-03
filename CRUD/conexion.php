c01f72fbc734fab72d2a753d5faa5c5d47e66ab9
KrisOP <kortega070@gmail.com>
2018-12-03 16:06:31 -0600

    corrigiendo error en conexion.php

--- CRUD/conexion.php
+++ CRUD/conexion.php
@@ -1,10 +1,11 @@
 <?php
     try {
         $base= new PDO ('mysql:host=localhost;dbname=pruebas', 'root','12345678');
-        $base->setAtribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $base->exc("SET CHARACTER SET UTF8");  
+       //$base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $base->exec("SET CHARACTER SET UTF8");  
     } catch (Exception $e) {
         die ('Error' .$e->getMessage());//cerrar la conexion y muestra el mensaje del error
-        echo "Linea del eroor". $e->getLine();//muestra la linea en el codigo donde se encuentra el error
+        echo "Linea del error". $e->getLine();//muestra la linea en el codigo donde se encuentra el error
     }
 ?>
