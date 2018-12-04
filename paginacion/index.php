b033b8acec1c68e146bc4a3ccbe3e1d16dbe48b1
KrisOP <kortega070@gmail.com>
2018-12-03 22:16:50 -0600

    algoritmo para paginacion de datos

--- paginacion/index.php
+++ paginacion/index.php
@@ -13,9 +13,30 @@
            //$base=new PDO('mysql:host=localhost;dbname=pruebas','root','12345678');
             $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $base->exec("SET CHARACTER SET utf8");
-            $sql_total="select SECCION, NOMBREARTICULO, PRECIO, PAIS from productos where seccion='deporte' LIMIT 0,3";//MUESTRAS SOLO LOS PRIMEROS 3 REGISTROS
-
+            $sql_total="select SECCION, NOMBREARTICULO, PRECIO, PAIS from productos where seccion='deporte'";//Con LIMIT 0,3 MUESTRAS SOLO LOS PRIMEROS 3 REGISTROS
+            $tamanio_paginas=3;//cuantos registros por pagina
+            $pagina=1;//la pagina al cargar la primera vez sera la pagina 1
+            
+            //para mostrar el inicio desde donde se va a tomar los registros para mostrarlos en la pagina
+            $empezar_desde=($pagina-1)*$tamanio_paginas;//al cargar la pagina por primera vez podemos utilizar esta variable en el LIMIT para hacer la consulta
+            
             $resultado=$base->prepare($sql_total);
+
+            $resultado->execute(array());
+
+
+            $num_filas=$resultado->rowCount();//devuelve cuantos registros a partir de la consulta
+            $total_paginas=ceil($num_filas/$tamanio_paginas);//redondea el resultado
+            
+            echo "Numero de registros de la consulta: ".$num_filas ."<br>";
+            echo "Mostramos ". $tamanio_paginas. "registros por paginas <br>";
+            echo "Mostrando la pagina ". $pagina. " de ". $total_paginas."<br> <br>"; 
+            
+            $resultado->closeCursor();
+
+            $sql_limite="select SECCION, NOMBREARTICULO, PRECIO, PAIS from productos where seccion='deporte' LIMIT $empezar_desde,3";//Con LIMIT 0,3 MUESTRAS SOLO LOS PRIMEROS 3 REGISTROS (del registro 0 al registro 3)
+            
+            $resultado=$base->prepare($sql_limite);
 
             $resultado->execute(array());
 
@@ -24,10 +45,9 @@
                 echo "Seccion: " .$registro["SECCION"]."Nombre Articulo: ". $registro["NOMBREARTICULO"]."Precio: ".$registro["PRECIO"]."Pais: ".$registro["PAIS"]."<br>";
             }
 
-            $resultado->closeCursor();
+            
 
         } catch (Exception $e) {
-            //throw $th;
             echo "Linea de Error". $e->getLine();
         }
     ?>
