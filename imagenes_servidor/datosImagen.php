cda8ee571f94a85f506f183b2d30e051ffba1740
KrisOP <kortega070@gmail.com>
2018-12-05 17:47:07 -0600

    instrucciones para subir imagenes las rutas de imagenes a la base de datos

--- imagenes_servidor/datosImagen.php
+++ imagenes_servidor/datosImagen.php
@@ -27,4 +27,24 @@ else{
 
     echo "el tamanio es demasiado grande";
 }
+
+require("datos_conexion.php");
+
+
+$conexion=mysqli_connect($db_host,$db_usuario,$db_contra);
+
+if (mysqli_connect_errno())//si existe algun error de conexion
+{
+    echo "Fallo al conectar con la BBDD";
+    exit();
+}
+
+mysqli_select_db($conexion,$db_nombre) or die ("No se encuentra la base de datos");
+
+mysqli_set_charset($conexion,"utf8");//para que se reconozca los simbolos latinos como el acento, la e;e
+//$sql="INSERT INTO productos (FOTO) VALUES ('$nombre_imagen')";
+$sql="UPDATE PRODUCTOS SET FOTO='$nombre_imagen' WHERE SECCION='DEPORTE'";
+$resultado=mysqli_query($conexion,$sql);
+
+
 ?>
