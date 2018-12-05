ad69bca48197d279d1b6c32fdc834c45d087e18b
KrisOP <kortega070@gmail.com>
2018-12-05 09:18:14 -0600

    incluyendo clases y funciones en el controlador

--- MVC/controlador/productos_controlador.php
+++ MVC/controlador/productos_controlador.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+require_once("../modelo/productos_modelo.php");
+//llamar al getter y ejecutar el constructos del modelo
+$producto = new Productos_model ();//instanciar la clase en el objeto
+
+$matrizProducto=$producto->get_productos();
+
+require_once("../vista/productos_view.php");
+
+?>
