bb3911754cada335e5b8bcef47a59bc39f895417
KrisOP <kortega070@gmail.com>
2018-12-04 19:32:24 -0600

    agregando modelos para ejecutar consultas utiliando POO

--- MVC/modelo/productos_modelo.php
+++ MVC/modelo/productos_modelo.php
@@ -1,12 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+class Productos_model{
+
+    private $db;//para almacenar la conexion
+    private $productos;
+
+    public function __construct(){
+
+        require_once("conexion.php");
+        $this->db=Conectar::conexion();//clase conectar y llamamos al metodo estatico conexion()
+        $this->productos=array();//la variable producto es un array
+    }
+
+    public function get_productos()//metodo getter para devolver los productos
+    {
+        $consulta=$this->db->query("SELECT * FROM productos"); //this->db es la conexion//luego query para la ejecucion de una comsulta 
+        
+        while ($fila=$consulta->fetch(PDO::FETCH_ASSOC)){//PARA LEER ARREGLOS ASOCIATIVOS
+
+            $this->productos[]=$filas;//almacenar cada uno de los registros al array productos
+        }
+
+        return $this->productos;
+    }
+
+}
+?>
