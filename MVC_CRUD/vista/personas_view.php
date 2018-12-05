2195c17b84a7c7435729a7ab4cd073186f2faaa4
KrisOP <kortega070@gmail.com>
2018-12-05 10:33:02 -0600

    depurando personas_view.php

--- MVC_CRUD/vista/personas_view.php
+++ MVC_CRUD/vista/personas_view.php
@@ -10,13 +10,44 @@
 
 <table>
 <tr><td>NOMBRE DEL ARTICULO</td>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+  <table width="50%" border="0" align="center">
+    <tr >
+      <td class="primera_fila">Id</td>
+      <td class="primera_fila">Nombre</td>
+      <td class="primera_fila">Apellido</td>
+      <td class="primera_fila">Dirección</td>
+      <td class="sin">&nbsp;</td>
+      <td class="sin">&nbsp;</td>
+      <td class="sin">&nbsp;</td>
+    </tr> 
+   
+		
     <?php
+      foreach ($matrizPersonas as $gente)://por cada objeto "gente" hay en el arreglo "registro" repetir el foreach?>
+
+   	<tr>
+      <td><?php echo $gente["id"]//mostrando cada campo del registro?></td>
+      <td><?php echo $gente["nombre"]?></td>
+      <td><?php echo $gente["apellido"]?></td>
+      <td><?php echo $gente["direccion"]?></td>
  
-        foreach ($matrizProducto as $registro) {
-           echo "<tr><td>". $registro["NOMBREARTICULO"]. "</td></tr>";
-        } 
-    
-    ?>
+      <td class="bot"><a href="borrar.php?id=<?php echo $gente->id//pasando por la url un id ("?id=php echo...")?>"><input type='button' name='del' id='del' value='Borrar'></a></td>
+      <td class='bot'><a href="editar.php?id=<?php echo $gente->id?> & nom=<?php echo $gente->nombre?> & ape=<?php echo $gente->apellido?> & dir=<?php echo $gente->direccion?>"><input type='button' name='up' id='up' value='Actualizar'></a></td>
+    </tr>  
+  <?php
+
+    endforeach;//cerrando el foreach. Usando esta nomenclatura facilita la concatenacion de html con php
+  ?>
+
+	<tr>
+	<td></td>
+      <td><input type='text' name='Nom' size='10' class='centrado'></td>
+      <td><input type='text' name='Ape' size='10' class='centrado'></td>
+      <td><input type='text' name='Dir' size='10' class='centrado'></td>
+      <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td></tr>    
+  </table>
+</form>
 
 </table>    
 </body>
