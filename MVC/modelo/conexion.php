44ad60be5a5145ec92d66e235852aaae7f94fa51
KrisOP <kortega070@gmail.com>
2018-12-04 19:16:01 -0600

    conexion a base de datos

--- MVC/modelo/conexion.php
+++ MVC/modelo/conexion.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+class Conectar{
+
+    public static function conexion()
+    {
+        try {
+            $conexion= new PDO ('mysql:host=localhost;dbname=pruebas', 'root','12345678');
+           //$base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $conexion->exec("SET CHARACTER SET UTF8");  
+        } catch (Exception $e) {
+            die ('Error' .$e->getMessage());//cerrar la conexion y muestra el mensaje del error
+            echo "Linea del error". $e->getLine();//muestra la linea en el codigo donde se encuentra el error
+        }
+
+        return $conexion;//devolver si la conexion ha tenido exito
+    }
+}
+
+
+?>
